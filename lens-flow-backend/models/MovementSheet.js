a20a5585eece6c4c3f4d062aed2505cb1af86cf6
Yuri-Diego <contatoprofissional.ya@gmail.com>
2025-03-29 18:16:33 -0300

    MovementSheet: Service, initial version without testing

--- lens-flow-backend/models/MovementSheet.js
+++ lens-flow-backend/models/MovementSheet.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const movementSheetSchema = new mongoose.Schema({
+    business: { type: mongoose.Schema.Types.ObjectId, ref: 'Business', required: true }, // Relacionamento N:1 com Business
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, },
     movements: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movement' }], // Relacionamento 1:N com Movement
-    business: { type: mongoose.Schema.Types.ObjectId, ref: 'Business', required: true }, // Relacionamento N:1 com Business
 });
 
 const MovementSheet = mongoose.model('MovementSheet', movementSheetSchema);
