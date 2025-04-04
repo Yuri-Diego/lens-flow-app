36ed9d8d0520b74835b822d345b27929eb6ba01b
Yuri-Diego <contatoprofissional.ya@gmail.com>
2025-04-03 22:49:51 -0300

    hotfix: business Id on movementSheet

--- lens-flow-backend/models/MovementSheet.js
+++ lens-flow-backend/models/MovementSheet.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const movementSheetSchema = new mongoose.Schema({
-    business: { type: mongoose.Schema.Types.ObjectId, ref: 'Business', required: true }, // Relacionamento N:1 com Business
+    businessId: { type: mongoose.Schema.Types.ObjectId, ref: 'Business', required: true }, // Relacionamento N:1 com Business
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, },
     movements: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movement' }], // Relacionamento 1:N com Movement
